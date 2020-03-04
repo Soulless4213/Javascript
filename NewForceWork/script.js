324f63099b14b225248aac77d277efb75bd5901b
Soulless4213 <49252372+Soulless4213@users.noreply.github.com>
2020-03-04 11:01:16 -0500

    Add files via upload

---
+++ NewForceWork/script.js
@@ -0,0 +1,31 @@
+//alert("Hello world")
+
+
+//var dillonObject = {
+//  firstName: "Dillon",
+//  lastName: "Vance",
+//  age: 21,
+//  likesToTravel: false,
+//  favoriteGames: ['MSG4','MGS5','Zelda']
+//}
+//
+//console.log(dillonObject.firstName); 
+//console.log(dillonObject.lastName); 
+//console.log(dillonObject.age); 
+//
+//if (dillonObject.likesToTravel === true){
+//	console.log("I like to travel.");
+//} else if (dillonObject.likesToTravel === false){
+//	console.log("I do not like to travel.");
+//}
+
+var favoriteFood = "chicken"
+
+console.log("My favorite food is " + favoriteFood + "!")
+
+if (favoriteFood === "fried pickles"){
+	console.log("You have excellent taste buds!")
+} else if (favoriteFood !== "fried pickles"){
+	console.log("You have horrible taste buds!")
+}
+
