718729280971b319e47589e341432eca7996c3d4
Soulless4213 <49252372+Soulless4213@users.noreply.github.com>
2020-03-04 11:45:28 -0500

    Add files via upload
    Added chapter 7 exercises

---
+++ NewForceWork/NewForceWork/script.js
@@ -0,0 +1,60 @@
+//alert("Hello world")
+
+
+//var dillonObject = {
+//  firstName: "Dillon",
+//  lastName: "Vance",
+//  age: 21,
+//  likesToTravel: false,
+//  favoriteGames: ['MSG4','MGS5','Zelda']
+//}
+//
+//console.log(dillonObject.firstName); 
+//console.log(dillonObject.lastName); 
+//console.log(dillonObject.age); 
+//
+//if (dillonObject.likesToTravel === true){
+//	console.log("I like to travel.");
+//} else if (dillonObject.likesToTravel === false){
+//	console.log("I do not like to travel.");
+//}
+
+//>>>>>>>Practice Chapter 7 Exercise 1 
+//var weatherObject = {
+//	temperature: 34,
+//	conditions: "Almost freezing",
+//	sunriseTime: "6:45am",
+//	sunsetTime: "7:00pm"
+//}
+
+
+//console.log(weatherObject.temperature);
+//console.log(weatherObject.conditions);
+//console.log(weatherObject.sunriseTime);
+//console.log(weatherObject.sunsetTime);
+
+//>>>>>>>>>>>>>>>>>>>>>Practice Chapter 7 Exercise 2
+
+//var highTemperatures = [55, 57, 53, 47, 56, 50];
+//var lowTemperatures = [33, 30, 29, 35, 31, 32]; 
+//
+//console.log(highTemperatures[0]);
+//console.log(lowTemperatures[5]);
+//console.log("The average high temperature is " + (55+57+53+47+56+50) / 6);
+//console.log("The average low temperature is " + (33+30+29+35+31+32) / 6);
+
+
+
+
+
+
+//var favoriteFood = "chicken"
+//
+//console.log("My favorite food is " + favoriteFood + "!")
+//
+//if (favoriteFood === "fried pickles"){
+//	console.log("You have excellent taste buds!")
+//} else if (favoriteFood !== "fried pickles"){
+//	console.log("You have horrible taste buds!")
+//}
+
